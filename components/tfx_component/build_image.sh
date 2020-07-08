f43a58626198b6a30aa48b76cd4c1aeee6447b67
mcamack <mcamack@gmail.com>
2020-07-07 17:12:37 -0700

    restructured dirs - tfx setup

---
+++ components/tfx_component/build_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 034557894042.dkr.ecr.us-west-2.amazonaws.com
+
+ecr_url=034557894042.dkr.ecr.us-west-2.amazonaws.com # Specify the image name here
+ecr_repo_name=kubeflow
+image_tag=latest
+full_image_name=${ecr_url}/${ecr_repo_name}:${image_tag}
+
+cd "$(dirname "$0")" 
+docker build -t "${ecr_repo_name}" .
+docker tag "${ecr_repo_name}:${image_tag}" "${full_image_name}"
+docker push "${full_image_name}"
+
+# Output the strict image name (which contains the sha256 image digest)
+docker inspect --format="{{index .RepoDigests 0}}" "${full_image_name}"
